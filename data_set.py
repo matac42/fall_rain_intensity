e85241adb1d696c11a50fe49a32078498c6eecdc
Yuto Matayoshi <e185742@ie.u-ryukyu.ac.jp>
2019-12-09 19:57:06 +0900

    add function 'data extract every 10sec'

--- data_set.py
+++ data_set.py
@@ -1,6 +1,7 @@
 import csv
 import re
-#Ï
+
+#10秒単位のカウンターの動きを時間の経過のように振る舞わせる．例) 60sの次は70sではなく1m00sにする．
 def clock(counter_per_ten):
     str_counter_per_ten = str(counter_per_ten)
     try:
@@ -10,28 +11,31 @@ def clock(counter_per_ten):
             counter_per_ten += 4000
     except:
         pass
+    return counter_per_ten
+
 
 with open('/Users/e185742/rain_fall/RxData/200906/20090601/192.168.100.9_csv.log', 'r') as f:
     reader = csv.reader(f)
-    ignore = 0
-    counter_per_ten = 0
+    #counter_per_ten: 10ごとにカウントする. 初期値は抽出開始時刻.
+    counter_per_ten = 10
+    former_row = csv
     for row in reader:
-        #最初の2行を無視
-        if ignore >= 2:
+        try:
             #1行ずつ読み込む
             time = row[0]
             time_no_coron = int(time[-8:-6]+time[-5:-3]+time[-2:])
             #10秒ずつ抽出
-            #counter_per_ten: 抽出開始時間
-            if time_no_coron <= counter_per_ten:
-                if time_no_coron == counter_per_ten:
-                    print(row)
-                else:
-                    #10秒単位で一致するデータがなかった時の処理
-                    #1秒前に戻ってそれを代替データとして採用する．
-                    pass
-            else:
+            if time_no_coron == counter_per_ten:
+                print(row)
                 counter_per_ten += 10
-                clock(counter_per_ten)
-        ignore += 1
-
+                counter_per_ten = clock(counter_per_ten)
+            else:
+                if time_no_coron > counter_per_ten:
+                    counter_per_ten += 10
+                    counter_per_ten = clock(counter_per_ten)
+                    print(former_row)
+                else:
+                    pass
+        except:
+            pass
+        former_row = row
