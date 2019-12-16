8e37c51fc24c9c557c7da7052cf3ef17a0817101
Yuto Matayoshi <e185742@ie.u-ryukyu.ac.jp>
2019-12-16 16:55:16 +0900

    make Frequency distribution

--- rain_data.py
+++ rain_data.py
@@ -11,18 +11,44 @@ def save_file_at_new_dir(new_dir_path, new_filename, new_file_content):
         f.write("\n")
 
 
+def judge_range(rain_data, data_list):
+    rain_amount = 0.
+    rain_data = float(rain_data)
+    while rain_data > rain_amount:
+        rain_amount += 3.
+    if rain_data == 0:
+        data_list[int(rain_amount/3.)] += 1
+    else:
+        data_list[int(rain_amount/3.)-1] += 1
+    
+    return data_list
+
+
 rainData = sorted(glob('RainData/*/*_rain.csv'))
-except_count = 0
+# max_rain = 0.
 FILE_NAM = r'\d*_rain.csv'
+data_list = [0] * 50
 for file in rainData:
-    result_file = "result"+file
-    result_file_directory = re.sub(FILE_NAM, '', result_file)
+    # result_file = "result"+file
+    # result_file_directory = re.sub(FILE_NAM, '', result_file)
     with open(file, "r") as f:
         reader = csv.reader((line.replace('\0','') for line in f))
         for row in reader:
             try:
                 result = row
-                result[1] = str(int(row[1]) * 60)
-                save_file_at_new_dir(result_file_directory, result_file, result)
+                result[1] = str(float(row[1]) * 0.0083333 * 60.0)
+                data_list = judge_range(result[1], data_list)
+                # save_file_at_new_dir(result_file_directory, result_file, result)
+                # if max_rain < float(result[1]):
+                #     max_rain = float(result[1])
             except:
-                except_count += 1
+                pass
+# mum = 0
+file_name = "resultRainData"
+for data in data_list:
+    with open(file_name, "a") as f:
+        f.write(str(data)+"\n")
+
+    # mum+=data
+# print(mum)
+# print(max_rain)
