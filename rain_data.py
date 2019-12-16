e6b9841f8035478b8d5dbed3b5453574ab3800f8
Yuto Matayoshi <e185742@ie.u-ryukyu.ac.jp>
2019-12-16 20:19:59 +0900

    plot csv

--- rain_data.py
+++ rain_data.py
@@ -2,6 +2,8 @@ import csv
 import os
 from glob import glob
 import re
+import pandas as pd
+import matplotlib.pyplot as plt
 
 #ディレクトリと書き込み用ファイルの作成
 def save_file_at_new_dir(new_dir_path, new_filename, new_file_content):
@@ -26,12 +28,9 @@ def judge_range(rain_data, data_list):
 
 
 rainData = sorted(glob('RainData/*/*_rain.csv'))
-# max_rain = 0.
 FILE_NAM = r'\d*_rain.csv'
 data_list = [0] * 51
 for file in rainData:
-    # result_file = "result"+file
-    # result_file_directory = re.sub(FILE_NAM, '', result_file)
     with open(file, "r") as f:
         reader = csv.reader((line.replace('\0','') for line in f))
         for row in reader:
@@ -39,25 +38,28 @@ for file in rainData:
                 result = row
                 result[1] = str(float(row[1]) * 0.0083333 * 60.0)
                 data_list = judge_range(result[1], data_list)
-                # save_file_at_new_dir(result_file_directory, result_file, result)
-                # if max_rain < float(result[1]):
-                #     max_rain = float(result[1])
             except:
                 pass
-# mum = 0
+
 file_name = "resultRainData.csv"
 accumulation = 0
 all_data = 0
 upper_limit = 150
 for data in data_list:
     all_data += data
+with open(file_name, "a") as f:
+    f.write("CumulativeTimeRate,RainfallIntensity\n")
 for data in reversed(data_list):
     accumulation += data
     ratio = float(accumulation) / float(all_data) * 100
     with open(file_name, "a") as f:
         f.write(str(upper_limit)+","+str(ratio)+"\n")
     upper_limit -= 3
-print(all_data)
-    # mum+=data
-# print(mum)
-# print(max_rain)
+
+# プロット
+df = pd.read_csv('resultRainData.csv', index_col="RainfallIntensity")
+plt.plot(df)
+plt.xscale('log')
+plt.xlabel('Cumulative time rate(%)')
+plt.ylabel('Rainfall intensity(mm/h)')
+plt.show()
